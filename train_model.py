f6f4d60c6cd2152e2ac6dab4eb33c0dadc54264e
Dhruvit Vala <dhruvit.vala2005@gmail.com>
2026-02-05 10:11:45 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,23 +2,41 @@ import os
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("diabetes.csv")
 
 if not os.path.exists("model.pkl"):
     
-    df.drop(['DiabetesPedigreeFunction'], axis=1, inplace=True)
-
-    X = df[['Pregnancies',	'Glucose',	'BloodPressure',	'SkinThickness',	'Insulin',	'BMI',	'Age']]
+    X = df.drop(['Outcome', 'DiabetesPedigreeFunction'], axis=1)
     y = df['Outcome']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    clf = GaussianNB()
-    clf.fit(X_train,y_train)
+    num_features = X.select_dtypes(exclude='object').columns
+    
+    num_transformer = Pipeline(steps=[
+    ('scaling', StandardScaler())
+    ])
 
-    joblib.dump(clf,"model.pkl")
+    preprocessor = ColumnTransformer(transformers=[
+    ('num', num_transformer, num_features)
+    ], remainder='drop')
+
+    model = GaussianNB()
+
+    final_model = Pipeline(steps=[
+    ('preprocesssing', preprocessor),
+    ('model', model)
+    ])
+    
+    final_model.fit(X_train,y_train)
+
+    joblib.dump(final_model,"model.pkl")
+    
     print("Model trained and saved as model.pkl")
 
 else:
